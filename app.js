b76f59a7e02eb8da9d7aa974f30528145667a8f7
Matias <raulmatiaspereyra@gmail.com>
2026-04-23 18:23:43 -0300

    feat: carrito con suma, resta y eliminación de productos

--- app.js
+++ app.js
@@ -5,11 +5,9 @@ const methodOverride = require("method-override");
 const app =  express();
 const PORT=3000;
 
-
 // Importa el archivo productos.json desde la carpeta data
 // y lo asigna a la constante 'productos' para usarlo en el código
 const productos = require('./data/productos.json');
-
 
 // Habilita el middleware para procesar datos enviados desde formularios HTML.
 // Con extended:true usa la librería 'qs', lo que permite interpretar objetos y arrays en req.body.
@@ -43,16 +41,48 @@ app.get ("/sobre",(req,res)=>{
 // Ruta POST de prueba
 app.post("/carrito", (req, res) => {
   console.log("Contenido de req.body:", req.body);
-
   const seleccionados = req.body.productos || [];
-  console.log("Array de productos seleccionados:", seleccionados);
-
+ //console.log("Array de productos seleccionados:", seleccionados);
   seleccionados.forEach(id => {
     const producto = productos.find(p => p.id === parseInt(id));
-    if (producto) carrito.push(producto);
+//Si el producto ya existe en el carrito, se incrementa su cantidad.Si no existe, se agrega como un nuevo item con cantidad 1
+      if (producto){
+        const item=carrito.find(p=>p.producto.id===producto.id);
+        if(item){
+            item.cantidad +=1;
+        }else{
+            carrito.push({producto,cantidad: 1});
+        }
+      }
+  
   });
+  //console.log("Carrito actualizado:", carrito);
+  res.redirect("/carrito");
+});
 
-  console.log("Carrito actualizado:", carrito);
+app.post("/carrito/sumar/:id",(req,res)=>{
+const id = parseInt(req.params.id);
+const item = carrito.find(p => p.producto.id === id);
+    if(item){
+  item.cantidad += 1;
+}
+  res.redirect("/carrito");
+});
+
+app.post("/carrito/restar/:id", (req, res) => {
+  const id = parseInt(req.params.id);
+
+  const item = carrito.find(p => p.producto.id === id);
+
+  if (item) {
+    if (item.cantidad > 1) {
+      // Restar si hay más de 1
+      item.cantidad -= 1;
+    } else {
+      // Si queda en 1 → eliminar
+      carrito = carrito.filter(p => p.producto.id !== id);
+    }
+  }
 
   res.redirect("/carrito");
 });
@@ -60,25 +90,28 @@ app.post("/carrito", (req, res) => {
 // Eliminar producto del carrito (DELETE): recibe el ID y lo filtra
 app.delete("/carrito/:id", (req, res) => {
   const id = parseInt(req.params.id);
-  carrito = carrito.filter(p => p.id !== id);
-
+  carrito = carrito.filter(item => item.producto.id !== id);
   console.log("Carrito actualizado:", carrito);
-
   res.redirect("/carrito");
 });
 
-
-
 // Mostrar carrito
 app.get("/carrito", (req, res) => {
-  res.render("carrito", { carrito });
+    const total=carrito.reduce((acc,item) =>{
+        return acc+item.producto.precio * item.cantidad;
+    },0);
+  res.render("carrito", { carrito,total });
 });
 
-
-
-
-
-
+app.get("/producto/:id",(req,res)=>{
+    const id = parseInt(req.params.id);
+    const  producto = productos.find(p=>p.id ===id);
+   console.log("producto detalle: ",producto);
+    if (!producto){
+        return res.send("Producto no encontrado");
+    }
+     res.render ("producto",{producto});
+});
 
 //servidor
 app.listen(PORT,()=>{
